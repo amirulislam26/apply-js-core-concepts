d0cb47d0649b62ea73f6b3f61e9ef5533e3ce7d4
amirulislam26 <26techamirulislam@gmail.com>
2022-10-20 08:02:11 -0700

    factorial while added

---
+++ factorial-while.js
@@ -0,0 +1,13 @@
+function factorial(number){
+    let num = 1;
+    let result = 1;
+    while(num <= 7){
+        result = result * num;
+        num++;
+    }
+    return result;
+}
+
+
+const Love = factorial(7);
+console.log(Love);
